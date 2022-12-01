8b76bda7614289a056251618721c416e47900a93
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-12-01 13:25:57 +0200

    added inquirer's promt

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const FileSystem = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+import inquirer from 'inquirer';
+console.log(inquirer);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const FileSystem = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-FileSystem.writeFile("./index.html", generatePage(name, github), err => {
-  if (err) throw err;
+// FileSystem.writeFile("./index.html", pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-})
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// })
 
 
